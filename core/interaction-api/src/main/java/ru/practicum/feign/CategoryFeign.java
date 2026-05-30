232485d6689bff943531141138cb7d5a66dd11b4
munuprig <egorprygun@ya.ru>
2026-05-30 13:31:07 +0700

    2.1.13 вынесения одинаковы Feign

--- core/interaction-api/src/main/java/ru/practicum/feign/CategoryFeign.java
+++ core/interaction-api/src/main/java/ru/practicum/feign/CategoryFeign.java
@@ -1,6 +1,5 @@
 package ru.practicum.feign;
 
-
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
--- core/interaction-api/src/main/java/ru/practicum/feign/UserFeign.java
+++ core/interaction-api/src/main/java/ru/practicum/feign/UserFeign.java
@@ -1,5 +1,4 @@
-package ru.practicum.request.feign.user;
-
+package ru.practicum.feign;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
