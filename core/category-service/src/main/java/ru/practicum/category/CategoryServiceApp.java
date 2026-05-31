50d7fd2b6df80411da5c184783169bfceb28ea8d
munuprig <egorprygun@ya.ru>
2026-05-31 23:26:25 +0700

    2.3.8

--- core/category-service/src/main/java/ru/practicum/category/CategoryServiceApp.java
+++ core/category-service/src/main/java/ru/practicum/category/CategoryServiceApp.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "ru.practicum")
 @EnableFeignClients(basePackages = "ru.practicum")
 public class CategoryServiceApp {
     public static void main(String[] args) {
