edab7bc6aae9ce0342fce1108235e65e3ba43255
munuprig <egorprygun@ya.ru>
2026-05-31 23:58:36 +0700

    2.3.13

--- core/event-service/src/main/java/ru/practicum/comment/model/Comment.java
+++ core/event-service/src/main/java/ru/practicum/comment/model/Comment.java
@@ -3,7 +3,6 @@ package ru.practicum.comment.model;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
-import lombok.ToString;
 import ru.practicum.event.model.Event;
 
 import java.time.LocalDateTime;
