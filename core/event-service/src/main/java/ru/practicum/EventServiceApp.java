62138c852463969d5ba01cc4b86e3a901da3099f
munuprig <egorprygun@ya.ru>
2026-05-31 23:33:05 +0700

    2.3.9

--- core/event-service/src/main/java/ru/practicum/EventServiceApp.java
+++ core/event-service/src/main/java/ru/practicum/EventServiceApp.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableFeignClients
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "ru.practicum")
 public class EventServiceApp {
     public static void main(String[] args) {
         SpringApplication.run(EventServiceApp.class, args);
--- core/request-service/src/main/java/ru/practicum/RequestServiceApplication.java
+++ core/request-service/src/main/java/ru/practicum/RequestServiceApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableFeignClients
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "ru.practicum")
 public class RequestServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(RequestServiceApplication.class, args);
